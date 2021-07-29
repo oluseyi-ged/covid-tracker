8cf34890822bd7d4d35e5b87db3a38bff070adec
oluseyi-ged <oluseyi.ged@gmail.com>
2021-07-29 05:44:14 +0100

    World covid tracker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { MenuItem, FormControl, Select } from "@material-ui/core"
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option 2</MenuItem>
+            <MenuItem value="worldwide">Option 3</MenuItem>
+            <MenuItem value="worldwide">Option 4</MenuItem>
+          </Select>
+        </FormControl>
+        {/* Header */}
+        {/* Title + Select dropdown field */}
+
+        {/* InfoBoxes */}
+        {/* InfoBoxes */}
+        {/* InfoBoxes */}
+
+        {/* Table of countries plus cases */}
+        {/* Graph */}
+
+        {/* Map */}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
