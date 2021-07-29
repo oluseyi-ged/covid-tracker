4de21fe696f5692f21fba2d024030294d61e1617
oluseyi-ged <oluseyi.ged@gmail.com>
2021-07-29 14:37:30 +0100

    setting up info boxes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react"
 import { MenuItem, FormControl, Select } from "@material-ui/core"
 import "./App.css"
+import InfoBox from "./InfoBox"
+import Map from "./Map"
 
 function App() {
   const [countries, setCountries] = useState([])
+  const [country, setCountry] = useState("worldwide")
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -21,36 +24,35 @@ function App() {
     getCountriesData()
   }, [])
 
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value
+    setCountry(countryCode)
+  }
+
   return (
     <div className="app">
       <div className="app__header">
         <h1>COVID-19 TRACKER</h1>
         <FormControl className="app__dropdown">
-          <Select variant="outlined" value="abc">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
             {/* loop through all countries and show a dropdown */}
-
+            <MenuItem value="worldwide">Worldwide</MenuItem>
             {countries.map((country) => (
               <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
-
-            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Option 2</MenuItem>
-            <MenuItem value="worldwide">Option 3</MenuItem>
-            <MenuItem value="worldwide">Option 4</MenuItem> */}
           </Select>
         </FormControl>
-        {/* Header */}
-        {/* Title + Select dropdown field */}
-
-        {/* InfoBoxes */}
-        {/* InfoBoxes */}
-        {/* InfoBoxes */}
-
-        {/* Table of countries plus cases */}
-        {/* Graph */}
-
-        {/* Map */}
       </div>
+
+      <div className="app__stats">
+        <InfoBox title="Covid Cases" cases="100k" total="500m" />
+        <InfoBox title="Recovered" cases="140k" total="250m" />
+        <InfoBox title="Death" cases="15k" total="10m" />
+      </div>
+      {/* Table of countries plus cases */}
+      {/* Graph */}
+
+      <Map />
     </div>
   )
 }
